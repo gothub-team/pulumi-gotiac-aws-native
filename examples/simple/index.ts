537dec9fd5e6e6eb70ffb9422eb6924bf863051f
Vincenz Koop <vincenz.koop@gmail.com>
2024-04-21 13:19:08 +0200

    added cloudfront keys

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,3 +1,8 @@
 import * as gotiac from "@pulumi/gotiac";
+import * as pulumi from "@pulumi/pulumi";
 
 const fileHosting = new gotiac.FileHosting("filehosting", { domain: "mediatest2.dev.gothub.io" } );
+
+export const url = pulumi.interpolate`https://${fileHosting.url}`;
+export const privateKeyId = fileHosting.privateKeyId;
+export const privateKeyParameterName = fileHosting.privateKeyParameterName;
