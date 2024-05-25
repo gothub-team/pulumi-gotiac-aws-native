979712530cc2f0fe5e34e0bccdc81162ce1d8f3b
Vincenz Koop <vincenz.koop@gmail.com>
2024-05-25 12:41:07 +0200

    user test finished

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,11 +1,20 @@
-import * as gotiac from "@pulumi/gotiac";
+import * as gotiac from "@gothub/pulumi-gotiac-aws";
 import * as pulumi from "@pulumi/pulumi";
 
-const fileHosting = new gotiac.FileHosting("filehosting", {
-    domain: "media.dev.gothub.io",
-    bucketName: 'gothub-dev-media',
-});
+// const fileHosting = new gotiac.FileHosting("filehosting", {
+//     domain: "media2.dev.gothub.io",
+// });
 
-export const url = pulumi.interpolate`https://${fileHosting.url}`;
-export const privateKeyId = fileHosting.privateKeyId;
-export const privateKeyParameterName = fileHosting.privateKeyParameterName;
+// const user = new gotiac.MailUser('MailUser', {
+//     region: 'eu-west-1',
+//     domain: 'dev.gothub.io',
+//     displayName: 'Info',
+//     name: 'Info',
+//     emailPrefix: 'info',
+//     enabled: false,
+//     // passwordSeed: 'abc',
+// });
+
+// export const url = pulumi.interpolate`https://${fileHosting.url}`;
+// export const privateKeyId = fileHosting.privateKeyId;
+// export const privateKeyParameterName = fileHosting.privateKeyParameterName;
